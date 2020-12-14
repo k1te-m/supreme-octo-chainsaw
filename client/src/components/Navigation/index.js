796e7d0156b6b31792bc4c11cb3524f3c4a04fed
k1te <kmiller343@gmail.com>
2020-12-14 16:38:18 -0600

    Created basic nav component

---
+++ client/src/components/Navigation/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <a>Home</a>
+            <a>Portfolio</a>
+            <a>Contact</a>
+            <a>Resume</a>
+            <a>LinkedIn</a>
+            <a>GitHub</a>
+        </div>
+    )
+};
+
+export default Navigation;
