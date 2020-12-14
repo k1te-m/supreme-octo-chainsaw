c086b44d56f17abd881d93ef2b396bc4d4ba2ec0
k1te <kmiller343@gmail.com>
2020-12-14 16:37:26 -0600

    updated app return

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from './pages/Landing';
+import GlobalStyle from "./components/GlobalStyle"
+import Loading from "./components/Loading"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle />
+    {/* <Loading /> */}
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
