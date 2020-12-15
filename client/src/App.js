861f615be0d48523ba755eff6d8d9840e470f0f2
k1te <kmiller343@gmail.com>
2020-12-15 15:34:56 -0600

    added rotue for about

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from './pages/Landing';
 import GlobalStyle from "./components/GlobalStyle"
+import About from './components/About';
 
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Landing />
+        </Route>
+        <Route exact path="/about">
+          <About />
         </Route>
       </Switch>
     </Router>
