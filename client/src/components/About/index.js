d436be0e4489c6825d9ebdc5aa8930a8ad0d4328
k1te <kmiller343@gmail.com>
2020-12-15 12:01:20 -0600

    updated about container/styles

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,14 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import NavButton from "../NavButton";
 
 const About = () => {
-    return(
-        <>
-            <div className="container">
-                <div>Hello</div>
-            </div>
-        </>
-        
-    )
+  return (
+    <>
+      <AboutHeader>Hello</AboutHeader>
+    </>
+  );
+};
+
+const AboutContainer = styled.div`
+height: 100vh;
+`
+
+const AboutHeader = styled.p`
+  font-size: 75px;
+  color: #dd8f29;
+  padding-top: 175px;
+  @media (min-width: 1200px) {
+    margin-top: auto;
 }
+`;
 
 export default About;
+
