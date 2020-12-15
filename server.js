4de3b8b163f405620206ed4d490dd07bb04b594d
k1te <kmiller343@gmail.com>
2020-12-15 15:58:09 -0600

    updated server for mongodb connection

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+// Dependencies
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+
+//Express instance
 const app = express();
+
+// Variable Port
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/newportfolio", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Start the Server
+app.listen(PORT, () => {
+  if (process.env.NODE_ENV !== "production") {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  }
 });
