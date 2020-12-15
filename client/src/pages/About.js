496d1f25c5c69bac9d95d48a2f3086adc482e0a0
k1te <kmiller343@gmail.com>
2020-12-14 23:07:49 -0600

    basic about component

---
+++ client/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavButton from "../components/NavButton";
+import Navigation from "../components/Navigation";
+
+
+const About = () => {
+    return(
+        <>
+            <div className="container">
+                <div>Hello</div>
+            </div>
+        </>
+        
+    )
+}
+
+export default About;
