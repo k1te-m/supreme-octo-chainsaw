5b0395960e75f48ddb5e38e75fe2faadff94771d
k1te <kmiller343@gmail.com>
2020-12-16 15:49:56 -0600

    removed parent styled button

--- client/src/components/NavButton/index.js
+++ client/src/components/NavButton/index.js
@@ -1,24 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+const NavButton = () => {
+  return (
+        <i className="fas fa-bars" />
+  );
+};
 
-const NavButton = ({ navOn, children }) => {
-    return (
-        <React.Fragment>
-            <Button className="navToggle" type="button">
-                <i className="fas fa-bars" />
-            </Button>
-        </React.Fragment>
-    )
-}
-
-const Button = styled.button`
-float: right;
-font-size: 3rem;
-width: 75px;
-background: #45B08C;
-border: none;
-color: #1ADED7;`
 
 
 export default NavButton;
