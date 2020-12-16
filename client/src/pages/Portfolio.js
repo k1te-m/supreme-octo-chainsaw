9c57f7c88ef24b8d1cfc199be11a644dd4c212f5
k1te <kmiller343@gmail.com>
2020-12-16 15:49:19 -0600

    added loading wheel and nav button

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -2,41 +2,68 @@ import React, { useState, useEffect } from "react";
 import RepoCard from "../components/RepoCard";
 import API from "../utils/API";
 import styled from "styled-components";
-
-
+import Loading from "../components/Loading";
+import NavButton from "../components/NavButton";
 
 
 const Portfolio = () => {
-    const [repoState, setRepoState] = useState([]);
-    let repos = [];
+  const [loadWheel, setLoadWheel] = useState(true);
+  const [repoState, setRepoState] = useState([]);
+  let repos = [];
 
-    const getRepos = () => {
-        API.getRepos().then((response) => {
-          repos = response.data;
-          setRepoState(repos);
-        })
-      }
-      useEffect(() => {
-        getRepos();
-      }, [])
-      return (
+  const getRepos = () => {
+    API.getRepos().then((response) => {
+      repos = response.data;
+      setRepoState(repos);
+    });
+  };
+  useEffect(() => {
+    getRepos();
+    const myFace = setTimeout(() => {
+      setLoadWheel(false);
+    }, 1000);
+  }, []);
+
+  if (loadWheel === true) {
+    return (
+      <LoadContainer>
+        <Loading />
+      </LoadContainer>
+    );
+  } else {
+    return (
+      <>
+        <NavToggleButton>
+          <NavButton />
+        </NavToggleButton>
         <PortfolioContainer className="container-fluid portfolio">
           <div className="row row-cols-3">
-            
-              {repoState.map(repo => {
-                return (
-                  <RepoCard value={repo} key={repo._id} />
-                )
-              })}
-            
+            {repoState.map((repo) => {
+              return <RepoCard value={repo} key={repo._id} />;
+            })}
           </div>
         </PortfolioContainer>
-      );
-}
+      </>
+    );
+  }
+};
 
 const PortfolioContainer = styled.div`
-height: 100vh;
-overflow: auto;`
+  height: 100vh;
+  overflow: auto;
+`;
 
+const LoadContainer = styled.div`
+  overflow: hidden;
+`;
+
+const NavToggleButton = styled.button`
+  float: right;
+  font-size: 3rem;
+  width: 75px;
+  background: #45b08c;
+  border: none;
+  color: #1aded7;
+`;
 
 export default Portfolio;
