e79d87aef76ca2a14da3c59a93ba07856b927d7e
k1te <kmiller343@gmail.com>
2020-12-17 16:39:50 -0600

    removed overflow hidden from body

--- client/src/components/GlobalStyle.js
+++ client/src/components/GlobalStyle.js
@@ -3,11 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 body{
     background: #45B08C;
-    overflow: hidden;
-}
-
-.landing-head{
-    overflow: hidden;
+    height: 100%;
 }
 `
 
