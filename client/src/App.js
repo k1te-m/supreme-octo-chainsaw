57391d1553326b3965a94fdba42f7da121afc53c
k1te <kmiller343@gmail.com>
2020-12-17 16:39:41 -0600

    formatted

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Landing from './pages/Landing';
-import GlobalStyle from "./components/GlobalStyle"
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-
-
+import Landing from "./pages/Landing";
+import GlobalStyle from "./components/GlobalStyle";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-    <GlobalStyle/>
-    <Router>
-      <Switch>
-        <Route exact path="/">
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path="/">
             <Landing />
-        </Route>
-        <Route exact path="/portfolio">
-          <Portfolio />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-      </Switch>
-    </Router>
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
