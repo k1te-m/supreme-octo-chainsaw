bbdaf94c25e6e69d675745b7b68e65c854bc45b2
k1te <kmiller343@gmail.com>
2020-12-21 16:32:07 -0600

    removed old code/updated bg color

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -5,7 +5,7 @@ import "./style.css"
 
 const hamburgerStyle = {
     fontSize: "3rem",
-    color: "aqua",
+    color: "#E6AA68",
     border: "none",
     marginRight: "10px",
 }
@@ -23,7 +23,6 @@ const Navigation = () => {
         aria-label="Toggle navigation"
         style={hamburgerStyle}
       >
-        {/* <span className="navbar-toggler-icon"></span> */}
         <i className="fas fa-bars"></i>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
@@ -56,51 +55,6 @@ const Navigation = () => {
       </div>
     </nav>
   );
-
-  // console.log(show);
-  // if (show === false) {
-  //     return null;
-  // } else {
-  //     return (
-  //         <div className="container">
-  //             {children}
-  //         </div>
-  //     )
-
-  //   <div className="container" style={displayOptions} role="dialog">
-  //     <ul className="list-group">
-  //       <li className="list-group-item">
-  //         <Link to="/">Home</Link>
-  //       </li>
-  //       <li className="list-group-item">
-  //         <Link to="/portfolio">Portfolio</Link>
-  //       </li>
-  //       <li className="list-group-item">
-  //         <Link to="/contact">Contact</Link>
-  //       </li>
-  //       <li className="list-group-item">
-  //         <a href="https://github.com/k1te-m" target="_blank">
-  //           GitHub
-  //         </a>
-  //       </li>
-  //       <li className="list-group-item">
-  //         <a
-  //           href="https://www.linkedin.com/in/kmiller29/"
-  //           target="_blank"
-  //         >
-  //           LinkedIn
-  //         </a>
-  //       </li>
-  //       <li className="list-group-item">
-  //         <a
-  //           href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view?usp=sharing"
-  //           target="_blank"
-  //         >
-  //           Resume
-  //         </a>
-  //       </li>
-  //     </ul>
-  //   </div>
 };
 
 export default Navigation;
