78916c3cf803324d3e76903c84b08a4ae6e20318
k1te <kmiller343@gmail.com>
2020-12-21 13:38:32 -0600

    added animations/cards

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,77 +1,84 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
+import Navigation from "../Navigation";
+import ContactCard from "../ContactCard";
+import SideNav from "../SideNav";
+import PortfolioCard from "../PortfolioCard";
 
 const About = () => {
   return (
     <>
-      <AboutContainer className="container">
-        <AboutHeader>
-          Recent graduate of Northwestern University’s Full Stack Web
-          Development Program with a history of success in financial services.
-          Detail oriented professional looking to grow and develop skills even
-          further while adding value to both the organization and their clients.
-        </AboutHeader>
-        <div className="row">
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">
-            <Card className="card">
-              <img className="card-img-top" src="./images/portfoliocard.jpg" />
-              <h5 className="card-title">Portfolio</h5>
-              <p className="card-text">
-                GitHub repositories for projects and applications.
-              </p>
-              <a href="/portfolio" className="btn btn-warning">
-                View Repositories
-              </a>
-            </Card>
-          </div>
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">
-            <Card className="card">
-              <ContactImg className="card-img-top" src="./images/contact.png" />
-              <h5 className="card-title">Contact</h5>
-              <p className="card-text">Email: kmiller343@gmail.com</p>
-              <p className="card-text">Phone: (847) 987-9744</p>
-              <p>
-                <a className="card-text" href="https://www.github.com/k1te-m">
-                  GitHub
-                </a>
-              </p>
-              <p>
-                <a
-                  className="card-text"
-                  href="https://www.linkedin.com/in/kmiller29/"
-                >
-                  LinkedIn
-                </a>
-              </p>
-              <a href="/contact" className="btn btn-warning">
-                Contact Form
-              </a>
-            </Card>
-          </div>
-        </div>
-      </AboutContainer>
+      <SideNav />
+      <Navigation />
+      <SlideInUpDiv>
+        <AboutContainer className="container">
+          <AboutHeader>My name is Kevin Miller.</AboutHeader>
+        </AboutContainer>
+      </SlideInUpDiv>
+      <SlideInRightDiv>
+        <ContactCard />
+      </SlideInRightDiv>
+      <SlideInLeftDiv>
+        <PortfolioCard />
+      </SlideInLeftDiv>
     </>
   );
 };
 
 const AboutHeader = styled.p`
   font-size: 25px;
-  color: #dd8f29;
-
-  @media (min-width: 1200px) {
-    margin-top: auto;
-  }
-`;
-
-const Card = styled.div`
-  width: 20rem;
-  background-color: #dd8f29;
-`;
-
-const ContactImg = styled.img`
-  background-color: #45b08c;
+  color: #f4a261;
 `;
 
 const AboutContainer = styled.div``;
+
+const slideInUP = keyframes`
+from {
+  opacity: 0;
+  transform: translate3d(0, 500%, 0);
+  visibility: 'visible'
+}
+to {
+  opacity: 1;
+  transform: none;
+}
+`;
+
+const slideInRight = keyframes`
+from {
+  transform: translate3d(1000%, 0, 0);
+  visibility: 'visible'
+}
+to {
+  transform: translate3d(0, 0, 0);
+}
+`;
+
+const SlideInUpDiv = styled.div`
+  animation: ${slideInUP} 2s linear;
+`;
+
+const SlideInRightDiv = styled.div`
+  position: fixed;
+  top: 10%;
+  right: 10%;
+  animation: ${slideInRight} 3s linear;
+`;
+
+const slideInLeft = keyframes`
+from {
+  transform: translate3d(-1000%, 0, 0);
+  visibility: 'visible'
+}
+to {
+  transform: translate3d(0, 0, 0);
+}
+`;
+const SlideInLeftDiv = styled.div`
+  position: fixed;
+  bottom: 10%;
+  left: 10%;
+  animation: ${slideInLeft} 3s linear;
+`;
 
 export default About;
