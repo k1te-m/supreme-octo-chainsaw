d617b610c251b0d4b9e8ddb115993429cfd7d19e
k1te <kmiller343@gmail.com>
2020-12-22 14:41:33 -0600

    hover animations and css for sidenav

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -1,45 +1,75 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const SideNav = () => {
-    return (
-        <SideNavStyle>
-            <ul className="list-group">
-                <li className="list-group-item">
-                    <a href="https://github.com/k1te-m" target="_blank">
-                        <img src="../../images/GitHub-Mark-32px.png" />
-                    </a>
-                </li>
-                <li className="list-group-item">
-                    <a href="https://www.linkedin.com/in/kmiller29/" target="_blank">
-                        <img src="../../images/LI-In-Bug.png"/>
-                    </a>
-                </li>
-                <li className="list-group-item">
-                    <a href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view?usp=sharing" target="_blank">
-                        <i class="fas fa-file" />
-                    </a>
-                </li>
-            </ul>
-        </SideNavStyle>
-    )
-}
+  return (
+    <SideNavStyle>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <a href="https://github.com/k1te-m" target="_blank">
+            <img src="../../images/GitHub-Mark-32px.png" />
+          </a>
+        </li>
+        <li className="list-group-item">
+          <a href="https://www.linkedin.com/in/kmiller29/" target="_blank">
+            <img src="../../images/LI-In-Bug.png" />
+          </a>
+        </li>
+        <li className="list-group-item">
+          <a
+            href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view?usp=sharing"
+            target="_blank"
+          >
+            <i class="fas fa-file" />
+          </a>
+        </li>
+      </ul>
+    </SideNavStyle>
+  );
+};
+
+const pulse = keyframes`
+    from { transfrom: scale(1); }
+    50% { transform: scale(.85); }
+    to { transform: scale(1);}
+`;
 
 const SideNavStyle = styled.div`
   margin-top: 5rem;
   margin-left: 1rem;
   position: fixed;
-  top: 2.9rem;
+  top: -1rem;
   left: -1rem;
+  
+  @media (min-width: 576px) {
+    top: 1.5rem;
+  }
+  @media (min-width: 768px) {
+    top: 4.5rem;
+  }
+  @media (min-width: 992px) {
+    top: 7.5rem;
+  }
+  @media (min-width: 1200px) {
+  }
+
   ul {
     list-style: none;
+    border: 1px solid orange;
   }
   .list-group-item {
-    background-color: transparent;
+    background-color: #fffbbd;
+    border: 1px solid orange;
+  }
+  img:hover {
+    animation: ${pulse} 1s infinite;
+  }
+  i:hover {
+    animation: ${pulse} 1s infinite;
   }
   i {
-      color: black;
-      font-size: 2rem;
+    color: black;
+    font-size: 2rem;
   }
 `;
 
