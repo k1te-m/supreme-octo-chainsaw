e2b7562f9fb9a7dc6200896261bc8edeed7c1160
k1te <kmiller343@gmail.com>
2020-12-22 13:37:13 -0600

    updated CSS for contact/portfolio cards

--- client/src/components/ContactCard/index.js
+++ client/src/components/ContactCard/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const ContactCard = () => {
-  const Centered = {
+  const centered = {
     margin: "0 auto",
     textAlign: "center",
-    textDecoration: "underline",
   };
   return (
     <Card className="card">
@@ -15,8 +14,10 @@ const ContactCard = () => {
         alt="Card image cap"
       />
       <div className="card-body">
-        <h5 className="card-title" style={Centered}>Contact Me</h5>
-        <p className="card-text" style={{textAlign: "center"}}>
+        <CardTitle className="card-title" style={centered}>
+          Contact
+        </CardTitle>
+        {/* <p className="card-text" style={{textAlign: "center"}}>
           To get in touch, please utilize the below contact methods or send me a
           message via the button below.
         </p>
@@ -33,53 +34,69 @@ const ContactCard = () => {
           <a href="#" className="btn btn-warning">
             Contact
           </a>
-        </div>
+        </div> */}
       </div>
     </Card>
   );
 };
 
 const Card = styled.div`
-  width: 200px;
-  height: 200px;
-  overflow: auto;
+  width: 140px;
+  height: 140px;
   border: 2px solid #e6aa68;
   background: #fffbbd;
-  font-size: 11px;
-  @media(min-width: 576px) {
-      width: 300px;
-      height: 325px;
-      font-size: 11px;
-  };
-  @media(min-width: 768px) {
-    width: 350px;
-    height: 375px;
-    font-size: 15px;
-  };
-  @media(min-width: 992px) {
-    width: 450px;
-    height: 475px;
-    font-size: 20px;
-  };
-  @media(min-width: 1200px) {
-    width: 425px;
-    height: 400px;
-    font-size: 15px;
-  };  
+  @media (min-width: 576px) {
+    height: 200px;
+    width: 200px;
+  }
+  @media (min-width: 768px) {
+    height: 250px;
+    width: 250px;
+  }
+  @media (min-width: 992px) {
+    height: 300px;
+    width: 300px;
+  }
+  @media (min-width: 1200px) {
+  } ;
 `;
 
 const ContactImg = styled.img`
   height: 70px;
   width: 70px;
   margin: 0 auto;
-  @media(min-width: 992px) {
-    height: 120px;
-    width: 120px;
+  @media (min-width: 576px) {
+    height: 125px;
+    width: 125px;
   }
+  @media (min-width: 768px) {
+    height: 175px;
+    width: 175px;
+  }
+  @media (min-width: 992px) {
+    height: 225px;
+    width: 225px;
+  }
+  @media (min-width: 1200px) {
+  } ;
 `;
 
-const ListItem = styled.li`
-  background: #fffbbd;
-`;
+const CardTitle = styled.h1`
+@media (min-width: 576px) {
+ 
+}
+@media (min-width: 768px) {
+  font-size: 25px;
+}
+@media (min-width: 992px) {
+  font-size: 30px;
+}
+@media (min-width: 1200px) {
+} ;
+`
+
+// const ListItem = styled.li`
+//   background: #fffbbd;
+// `;
 
 export default ContactCard;
--- client/src/components/PortfolioCard/index.js
+++ client/src/components/PortfolioCard/index.js
@@ -2,47 +2,93 @@ import React from "react";
 import styled from "styled-components";
 
 const PortfolioCard = () => {
-  const Centered = {
+  const centered = {
     margin: "0 auto",
-    width: "50%",
+    textAlign: "center",
   };
   return (
-    <Card className="card card-block d-flex">
+    <Card className="card">
       <PortfolioImg
         className="card-img-top"
         src="../images/cloud.png"
         alt="Code Cloud"
       />
       <div className="card-body">
-        <h5 className="card-title">Portfolio</h5>
-        <p className="card-text">
+        <CardTitle className="card-title" style={centered}>
+          Portfolio
+        </CardTitle>
+        {/* <p className="card-text">
           GitHub repositories for projects and applications.
         </p>
         <div style={Centered}>
           <a href="#" className="btn btn-warning">
             View Portfolio
           </a>
-        </div>
+        </div> */}
       </div>
     </Card>
   );
 };
 
 const Card = styled.div`
-  width: 18rem;
-  border: 2px solid #E6AA68;
+  height: 140px;
+  width: 140px;
+  border: 2px solid #e6aa68;
   background: #fffbbd;
+  @media (min-width: 576px) {
+    height: 200px;
+    width: 200px;
+  }
+  @media (min-width: 768px) {
+    height: 250px;
+    width: 250px;
+  }
+  @media (min-width: 992px) {
+    height: 300px;
+    width: 300px;
+  }
+  @media (min-width: 1200px) {
+  } ;
 `;
 
-const ListItem = styled.li`
-  background: #fffbbd;
-`;
+const CardTitle = styled.h1`
+@media (min-width: 576px) {
+ 
+}
+@media (min-width: 768px) {
+  font-size: 25px;
+}
+@media (min-width: 992px) {
+  font-size: 30px;
+}
+@media (min-width: 1200px) {
+} ;
+`
+
+// const ListItem = styled.li`
+//   background: #fffbbd;
+// `;
+
 // Image Provided by flaticon.com
 const PortfolioImg = styled.img`
-  height: 50%;
-  width: 50%;
+  height: 70px;
+  width: 70px;
   margin: 0 auto;
-  padding-top: 5px;
+  padding: 3px;
+  @media (min-width: 576px) {
+    height: 125px;
+    width: 125px;
+  }
+  @media (min-width: 768px) {
+    height: 175px;
+    width: 175px;
+  }
+  @media (min-width: 992px) {
+    height: 225px;
+    width: 225px;
+  }
+  @media (min-width: 1200px) {
+  } ;
 `;
 
 export default PortfolioCard;
