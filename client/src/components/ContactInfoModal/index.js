7eee688854f03bc8382ebc96ee7304cba51c4dcd
k1te <kmiller343@gmail.com>
2020-12-28 13:21:23 -0600

    created contact info modal

---
+++ client/src/components/ContactInfoModal/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactInfoModal = ({ show, children }) => {
+  if (show === false) {
+    return null;
+  } else {
+    return <div className="modal-container">{children}</div>;
+  }
+};
+
+export default ContactInfoModal;
