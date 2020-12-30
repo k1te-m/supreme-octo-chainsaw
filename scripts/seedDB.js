e544c02642e85a174ab41d6a640490baea4e15a3
k1te <kmiller343@gmail.com>
2020-12-30 16:47:17 -0600

    updated seeds to include language/packages used

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -11,44 +11,50 @@ const repoSeed = [
         name: "explorer",
         github: "k1te-m",
         imageURL: "../images/explorer.PNG",
-        description: "This is a MERN stack web game that is designed to take the user on an educational journey while interacting with NASA information to gain explorative knowledge. Gain credits as rewards for answering correct quiz questions while transversing the universe. Purchase and upgrade ships throught your journey."
+        description: "This is a MERN stack web game that is designed to take the user on an educational journey while interacting with NASA information to gain explorative knowledge.",
+        languages: ["React.js", "MongoDB", "Express", "Node.js", "Bootstrap"],
     },
     {
         name: "EcommerceApp",
         github: "k1te-m",
         imageURL: "../images/limitelec.PNG",
-        description: "This is as mock ecommerce site. Product data is stored in a MySQL database, and the users can browse in a variety of ways. They can see all of our products on one page, they can refine their search to a particular category or brand of product via a dropdown menu, and they can search for all products that partially match a string (e.g., television). Users can optionally create an account, and log in and log out. While logged in, they can view their member information."
+        description: "This is as mock ecommerce site. Product data is stored in a MySQL database, and the users can browse in a variety of ways.",
+        languages: ["jQuery", "Handlebars", "MySQL", "Express", "Node.js", "Bootstrap",],
     },
     {
         name: "Employee-Directory",
         github: "k1te-m",
         imageURL: "../images/employeedirect.PNG",
-        description: "A React application that acts as an employee directory for employees or managers to be able to quickly view non-sensitive information about their colleagues."
+        description: "A React application that acts as an employee directory for employees or managers to be able to quickly view non-sensitive information about their colleagues.",
+        languages: ["React.js", "Express", "Node.js", "Bootstrap", ]
     },
-    {
-        name: "Restaurant-Roulette",
-        github: "k1te-m",
-        imageURL: "../images/restroulette.PNG",
-        description: "An application that returns a random restaurant selection based on the user city location via Zomato's API."
-    },
+    // {
+    //     name: "Restaurant-Roulette",
+    //     github: "k1te-m",
+    //     imageURL: "../images/restroulette.PNG",
+    //     description: "An application that returns a random restaurant selection based on the user city location via Zomato's API.",
+    //     languages: ["React.js", "Express", "Node.js", "Foundation", ]
+    // },
     {
         name: "Budget-Tracker",
         github: "k1te-m",
         imageURL: "../images/budget.PNG",
-        description: "Created a PWA from provided front-end and back-end files for a budget tracking app."
+        description: "Created a PWA from provided front-end and back-end files for a budget tracking app.",
+        languages: ["PWA", "Service Worker", ]
     },
     {
         name: "Employee-Tracker",
         github: "k1te-m",
         imageURL: "../images/employeetrack.PNG",
-        description: "This application is intended to be used as a Content Management System for a fictional company. Allowing non-developers the ability to view and interact with information stored in our database."
+        description: "This application is intended to be used as a Content Management System for a fictional company.",
+        languages: ["CLI", "MySQL", "Node.js",]
     },
-    {
-        name: "React-Portfolio",
-        github: "k1te-m",
-        imageURL: "../images/portfolioex.PNG",
-        description: "A React application to display some quick information about myself along with my resume, LinkedIn profile, and GitHub repositories."
-    },
+    // {
+    //     name: "React-Portfolio",
+    //     github: "k1te-m",
+    //     imageURL: "../images/portfolioex.PNG",
+    //     description: "A React application to display some quick information about myself along with my resume, LinkedIn profile, and GitHub repositories."
+    // },
 ]
 
 db.Repo.remove({})
