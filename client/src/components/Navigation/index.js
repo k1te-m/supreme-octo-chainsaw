2b83d66e6c07f4a8791c128c35fbd40066257d17
k1te <kmiller343@gmail.com>
2020-12-30 01:38:41 -0600

    CSS for navigation

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import { Link } from "react-router-dom";
-import "./style.css";
 
 const hamburgerStyle = {
   fontSize: "3rem",
@@ -9,6 +8,10 @@ const hamburgerStyle = {
   border: "none",
   marginRight: "10px",
 };
+
+const WhiteText = {
+  color: "#F2F2F2",
+}
 
 const Navigation = () => {
   return (
@@ -25,56 +28,63 @@ const Navigation = () => {
       >
         <i className="fas fa-bars"></i>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <div className="padding-values">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
+      <NavBarCollapse className="collapse navbar-collapse" id="navbarNav">
+        <CollapsedWrapper className="container">
+          <NavBackground className="background container">
+            <NavBarUL className="navbar-nav">
+              <NavBarLI className="nav-item">
+                <Link to="/" className="nav-link" style={WhiteText}>
+                  Home
+                </Link>
+              </NavBarLI>
 
-            <li className="nav-item">
-              <Link to="/portfolio" className="nav-link">Portfolio</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-link">
-                Contact
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://github.com/k1te-m"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www.linkedin.com/in/kmiller29/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view?usp=sharing"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+              <NavBarLI className="nav-item">
+                <Link to="/portfolio" className="nav-link" style={WhiteText}>
+                  Portfolio
+                </Link>
+              </NavBarLI>
+              <NavBarLI className="nav-item">
+                <Link to="/contact" className="nav-link" style={WhiteText}>
+                  Contact
+                </Link>
+              </NavBarLI>
+              <NavBarLI className="nav-item">
+                <a
+                  className="nav-link"
+                  href="https://github.com/k1te-m"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={WhiteText}
+                >
+                  GitHub
+                </a>
+              </NavBarLI>
+              <NavBarLI className="nav-item">
+                <a
+                  className="nav-link"
+                  href="https://www.linkedin.com/in/kmiller29/"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={WhiteText}
+                >
+                  LinkedIn
+                </a>
+              </NavBarLI>
+              <NavBarLI className="nav-item">
+                <a
+                  className="nav-link"
+                  href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view?usp=sharing"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={WhiteText}
+                >
+                  Resume
+                </a>
+              </NavBarLI>
+            </NavBarUL>
+          </NavBackground>
+        </CollapsedWrapper>
+      </NavBarCollapse>
     </NavNav>
   );
 };
@@ -93,5 +103,81 @@ const NavNav = styled.nav`
   animation: ${fadeIn} 2s ease-in-out;
   height: auto;
 `;
+
+const NavBarCollapse = styled.div`
+  width: 100%;
+`;
+
+const NavBarUL = styled.ul`
+  margin-top: 0px;
+  border: 1px solid #3B945E;
+  @media (orientation: landscape) and (max-height: 360px) {
+    margin-top: -250px;
+  }
+`;
+
+const NavBarLI = styled.li`
+  display: flex;
+  border: 1px solid #3B945E;
+  size: 15px;
+  background-color: #182628;
+  justify-content: center;
+  width: 300px;
+  @media (min-width: 576px) {
+    width: 500px;
+  }
+  @media (min-width: 768px) {
+    width: 700px;
+  }
+  @media (min-width: 992px) {
+    width: 900px;
+  }
+  @media (min-width: 1200px) {
+    width: 1150px;
+  } ;
+`;
+
+const CollapsedWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+  width: 300px;
+  padding: auto;
+  margin: auto;
+  @media (min-width: 576px) {
+    height: 700px;
+  }
+  @media (min-width: 768px) {
+    height: 950px;
+  }
+  @media (min-width: 992px) {
+    height: 1200px;
+  }
+  @media (min-width: 1200px) {
+    height: 800px;
+  } ;
+`
+
+const NavBackground = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 285px;
+  width: 350px;
+  background-color: #57BA98;
+  @media (min-width: 576px) {
+    width: 550px;
+  }
+  @media (min-width: 768px) {
+    width: 750px;
+  }
+  @media (min-width: 992px) {
+    width: 975px;
+  }
+  @media (min-width: 1200px) {
+    width: 1175px;
+  } ;
+`
 
 export default Navigation;
