2d6933e6df300194d8d871a089b49c777de6cd72
k1te <kmiller343@gmail.com>
2020-12-29 19:41:07 -0600

    updating font sizes/colors

--- client/src/components/LandingText/index.js
+++ client/src/components/LandingText/index.js
@@ -11,18 +11,16 @@ const LandingText = () => {
           </LandingHeader1>
         </div>
         <div className="row mt-0 pt-0 mb-5">
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">
-            <AboutDescription>
-              Pellentesque mollis erat lectus, a tempus ipsum consectetur quis.
-              Quisque mollis lorem in felis semper, in pulvinar lacus gravida.
-              Phasellus quis nibh pharetra ex tristique suscipit id a ex. Cras
-              vitae augue eget velit bibendum rutrum vitae sed ex. Duis ex sem,
-              consectetur et nunc ut, consequat pharetra dui. In placerat arcu
-              vitae luctus mattis. Suspendisse nec nisi quis urna viverra
-              feugiat at sed dui. Aliquam venenatis congue accumsan. Etiam ut
-              nunc tincidunt, efficitur ante sed, sollicitudin nibh.
-            </AboutDescription>
-          </div>
+          <AboutDescription>
+            Pellentesque mollis erat lectus, a tempus ipsum consectetur quis.
+            Quisque mollis lorem in felis semper, in pulvinar lacus gravida.
+            Phasellus quis nibh pharetra ex tristique suscipit id a ex. Cras
+            vitae augue eget velit bibendum rutrum vitae sed ex. Duis ex sem,
+            consectetur et nunc ut, consequat pharetra dui. In placerat arcu
+            vitae luctus mattis. Suspendisse nec nisi quis urna viverra feugiat
+            at sed dui. Aliquam venenatis congue accumsan. Etiam ut nunc
+            tincidunt, efficitur ante sed, sollicitudin nibh.
+          </AboutDescription>
         </div>
       </HeaderContainer>
     </>
@@ -32,18 +30,41 @@ const LandingText = () => {
 const AboutDescription = styled.p`
   color: #f2f2f2;
   text-align: center;
+  font-size: 14px;
+  @media (min-width: 576px) {
+    font-size: 18px;
+  }
+  @media (min-width: 768px) {
+    font-size: 22px;
+  }
+  @media (min-width: 992px) {
+    font-size: 26px;
+  }
+  @media (min-width: 1200px) {
+    
+  }
+
 `;
 
 const LandingHeader1 = styled.h1`
   text-align: center;
-  font-size: 50px;
+  font-size: 60px;
   color: #3b945e;
 
   margin-bottom: 0px;
   padding-bottom: 0px;
+  @media (min-width: 576px) {
+    font-size: 80px;
+  }
+  @media (min-width: 768px) {
+    font-size: 120px;
+  }
+  @media (min-width: 992px) {
+    font-size: 160px;
+  }
   @media (min-width: 1200px) {
     margin-top: auto;
-    font-size: 250px;
+    font-size: 200px;
   }
 `;
 
@@ -57,10 +78,14 @@ const fadeIn = keyframes`
 `;
 const HeaderContainer = styled.div`
   animation: ${fadeIn} 1.5s linear;
+  @media (orientation: landscape) {
+    margin-top: 25px;
+    height: auto;
+  }
 `;
 
 const Period = styled.span`
-  color: #3b945e;
+  color: #f2f2f2;
   -webkit-text-stroke-width: 1px;
   -webkit-text-stroke-color: #182628;
 `;
