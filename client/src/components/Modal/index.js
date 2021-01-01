7aa81a4ab93844e90c904847ff6740ca6bc274e6
k1te <kmiller343@gmail.com>
2021-01-01 15:09:43 -0600

    built animated modal with framer motion

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,24 +1,103 @@
 import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import styled from "styled-components";
 
-const Modal = ({ show, children }) => {
-  if (show === false) {
-    return null;
-  } else {
-    return (
-      <AnimatePresence>
-        <motion.div
-          className="modal-container"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 2 }}
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
-    );
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+`;
+
+const ModalContainer = styled(motion.div)`
+  width: 325px;
+  height: auto;
+  background-color: #182628;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 12px;
+  border: 1px solid #3b945e;
+  @media screen and (min-width: 576px) {
+    width: 450px;
   }
+  @media screen and (min-width: 768px) {
+    
+  }
+  @media screen and (min-width: 992px) {
+    width: 550px;
+  }
+  @media screen and (min-width: 1200px) {
+  }
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  right: 3%;
+  height: auto;
+  width: auto;
+  background: transparent;
+  border: none;
+  padding: 0;
+  margin: 0;
+  font-size: 1.5rem;
+  color: #65ccb8;
+`;
+
+const modalVariant = {
+  initial: { opacity: 0 },
+  isOpen: { opacity: 1 },
+  messageSent: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
+const containerVariant = {
+  initial: { top: "-50%", transition: { type: "spring" } },
+  isOpen: { top: "50%" },
+  messageSent: { top: "50%" },
+  exit: { top: "-50%" },
+};
+
+const Modal = ({ handleClose, children, isOpen, messageSent }) => {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <Overlay
+          initial={"initial"}
+          animate={"isOpen"}
+          exit={"exit"}
+          variants={modalVariant}
+        >
+          <ModalContainer variants={containerVariant}>
+            <CloseButton onClick={handleClose}>
+              <i class="far fa-times-circle" />
+            </CloseButton>
+            {children}
+          </ModalContainer>
+        </Overlay>
+      )}
+      {messageSent && (
+        <Overlay
+          initial={"initial"}
+          animate={"messageSent"}
+          exit={"exit"}
+          variants={modalVariant}
+        >
+          <ModalContainer variants={containerVariant}>
+            <CloseButton onClick={handleClose}>
+              <span aria-hidden="true">
+                <i class="far fa-times-circle" />
+              </span>
+            </CloseButton>
+            {children}
+          </ModalContainer>
+        </Overlay>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Modal;
