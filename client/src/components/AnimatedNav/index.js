f463ecde197b6ad05486b49bdd2b3b2c6174022e
k1te <kmiller343@gmail.com>
2021-01-03 12:56:48 -0600

    Created AnimatedNav Component

---
+++ client/src/components/AnimatedNav/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import styled from "styled-components";
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+`;
+const NavigationContainer = styled(motion.div)`
+  width: 90%;
+  height: 90%;
+  background-color: #182628;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const CloseButton = styled.button`
+  position: absolute;
+  right: 3%;
+  height: auto;
+  width: auto;
+  background: transparent;
+  border: none;
+  padding: 0;
+  margin: 0;
+  font-size: 1.5rem;
+  color: #65ccb8;
+`;
+const navVariant = {
+  initial: { opacity: 0 },
+  navOpen: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+const containerVariant = {
+  initial: { top: "-50%", transition: { type: "spring" } },
+  navOpen: { top: "50%" },
+  exit: { top: "-50%" },
+};
+
+const AnimatedNav = ({ handleClose, children, navOpen}) => {
+    return (
+        <AnimatePresence>
+            {navOpen && (
+                <Overlay
+                    initial={"initial"}
+                    animate={"navOpen"}
+                    exit={"exit"}
+                    variants={navVariant}
+                >
+                    <NavigationContainer variants={containerVariant}>
+                        <CloseButton onClick={handleClose}>
+                            <i class="far fa-times-circle" />
+                        </CloseButton>
+                        {children}
+                    </NavigationContainer>
+                </Overlay>
+            )}
+        </AnimatePresence>
+    )
+}
+
+export default AnimatedNav;
