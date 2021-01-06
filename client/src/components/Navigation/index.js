35f65f3ad327344f80033037fe3480bcd547819a
k1te <kmiller343@gmail.com>
2021-01-06 01:38:50 -0600

    rm keyframes

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import AnimatedNav from "../AnimatedNav";
 import { motion } from "framer-motion";
