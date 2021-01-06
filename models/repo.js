984f6595962f877b455fffa932e3d3829572355f
k1te <kmiller343@gmail.com>
2021-01-06 01:34:28 -0600

    updated model for deployed link

--- models/repo.js
+++ models/repo.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const repoSchema = new Schema({
     name: {type: String},
     github: {type: String},
+    deployedLink: { type: String },
     imageURL: {type: String},
     description: {type: String},
     languages: [String],
