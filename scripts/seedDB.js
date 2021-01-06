94a5935d8ff07eb9f510a779b97584e977d56d0c
k1te <kmiller343@gmail.com>
2021-01-06 01:34:15 -0600

    updated seeds to include deployed link

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -10,6 +10,7 @@ const repoSeed = [
     {
         name: "explorer",
         github: "k1te-m",
+        deployedLink: "https://gentle-atoll-92825.herokuapp.com/",
         imageURL: "../images/explorer.PNG",
         description: "This is a MERN stack web game that is designed to take the user on an educational journey while interacting with NASA information to gain explorative knowledge. Gain credits as rewards for answering correct quiz questions while transversing the universe. Purchase and upgrade ships throught your journey.",
         languages: ["React.js", "MongoDB", "Express", "Node.js", "Bootstrap"],
@@ -17,6 +18,7 @@ const repoSeed = [
     {
         name: "EcommerceApp",
         github: "k1te-m",
+        deployedLink: "https://fierce-earth-38835.herokuapp.com/",
         imageURL: "../images/limitelec.PNG",
         description: "This is as mock ecommerce site. Product data is stored in a MySQL database, and the users can browse in a variety of ways. Users can optionally create an account, and log in and log out. While logged in, they can view their member information.",
         languages: ["jQuery", "Handlebars", "MySQL", "Express", "Node.js", "Bootstrap",],
@@ -24,6 +26,7 @@ const repoSeed = [
     {
         name: "Employee-Directory",
         github: "k1te-m",
+        deployedLink: "https://fierce-falls-06822.herokuapp.com/",
         imageURL: "../images/employeedirect.PNG",
         description: "A React application that acts as an employee directory for employees or managers to be able to quickly view non-sensitive information about their colleagues.",
         languages: ["React.js", "Express", "Node.js", "Bootstrap", ]
@@ -38,6 +41,7 @@ const repoSeed = [
     {
         name: "Budget-Tracker",
         github: "k1te-m",
+        deployedLink: "https://thawing-crag-91029.herokuapp.com/",
         imageURL: "../images/budget.PNG",
         description: "Created a PWA from provided front-end and back-end files for a budget tracking app.",
         languages: ["PWA", "Service Worker", ]
@@ -45,6 +49,7 @@ const repoSeed = [
     {
         name: "Employee-Tracker",
         github: "k1te-m",
+        deployedLink: "https://github.com/k1te-m/Employee-Tracker",
         imageURL: "../images/employeetrack.PNG",
         description: "This application is intended to be used as a Content Management System for a fictional company. Allowing non-developers the ability to view and interact with information stored in our database.",
         languages: ["CLI", "MySQL", "Node.js",]
